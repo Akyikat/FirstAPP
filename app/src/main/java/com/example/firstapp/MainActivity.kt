e21b3f3db47160965806ea3586dc865a90e5f9f1
Akyikat <akyikat0304@gmail.com>
2020-10-28 16:20:16 +0600

    hw001

--- app/src/main/java/com/example/firstapp/MainActivity.kt
+++ app/src/main/java/com/example/firstapp/MainActivity.kt
@@ -7,48 +7,29 @@ import android.view.View
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-    /*
-   1. Всегда следить за пробелами и вложенностью
-   2. Между функциями должен быть отступ в одну строчку
-   3. Все переменные должны называться по смыслу
-     */
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-       // text_view.text = getName(60)
-       // Log.v("CREATED_NAME", getName(60))
-
-        changeTextAction()
+        getFirstButtonAction()
         
     }
 
-    private fun changeTextAction() {
-        change.setOnClickListener {
-            var value = edit_text.text.toString()
-            text_view.text = value
-         //   edit_text.setText("")
-            edit_text.text.clear()
-            change.setTextColor(resources.getColor(R.color.changeButtonText))
-
+    private fun getFirstButtonAction() {
+        button_plus.setOnClickListener{
+            var firstValue = first_value.text.toString().toInt()
+            var secondValue = second_value.text.toString().toInt()
+            var result = firstValue + secondValue
+            text_view.text="$result"
+            first_value.setText("")
+            second_value.setText("")
         }
     }
 
+
 }
-    //var - переменная age - название;
-    // Int - наследование или принадлежность к определенному типу,
-    // = 40 - значение
 
-    //var age: Int = 40
-   // var isMarried: Boolean = false
-   // var createdName: String = "Romeo"
-   // var size: Short = 35
-   // var length: Byte = 4
-   // var weight: Double = 3.785
 
-   // fun getName(age: Int): String {
-    //    return createdName + age
-  //  }
-//}
 
 
 // Что такое переменная? - ячейка памяти для хранения информации
