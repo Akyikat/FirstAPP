197a495c2382e0edb46c8b243afa32eaa3945037
Akyikat <akyikat0304@gmail.com>
2020-11-02 21:45:38 +0600

    hw 03

--- app/src/main/java/com/example/firstapp/MainActivity.kt
+++ app/src/main/java/com/example/firstapp/MainActivity.kt
@@ -2,34 +2,82 @@ package com.example.firstapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Message
 import android.util.Log
 import android.view.View
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+    var textArray = mutableListOf<String>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        getFirstButtonAction()
-        
+
+        addToArray()
+        removeFromArray()
     }
 
-    private fun getFirstButtonAction() {
-        button_plus.setOnClickListener{
-            var firstValue = first_value.text.toString().toInt()
-            var secondValue = second_value.text.toString().toInt()
-            var result = firstValue + secondValue
-            text_view.text="$result"
-            first_value.setText("")
-            second_value.setText("")
+     //textChangedListener
+
+    private fun addToArray(){
+        btnAdd.setOnClickListener {
+            val value = edText.text.toString()
+            if (value.isBlank() || value.isEmpty()) {
+                edText.text.clear()
+                Toast.makeText(this, "Нельзя добавить пустое значение", Toast.LENGTH_LONG).show()
+            } else if (edText.length() <= 3){
+                edText.text.clear()
+                Toast.makeText(this, "Значение должно быть больше 3-х символов.", Toast.LENGTH_LONG).show()
+            } else {
+                edText.text.clear()
+                textArray.add(value)
+                displayArray()
+            }
         }
     }
 
+    private fun removeFromArray(){
+        btnRemove.setOnClickListener {
+            val value = edText.text.toString()
+            if (value.isBlank() || value.isEmpty()) {
+                edText.text.clear()
+                Toast.makeText(this, "Введите значение", Toast.LENGTH_LONG).show()
+            } else {
+                edText.text.clear()
+                findAndRemoveFromArray(value)
+                displayArray()
+            }
+        }
+    }
 
+    private fun findAndRemoveFromArray(value: String) {
+        var indexOfArray: Int? = null
+        var elements = ""
+        for((i,text) in textArray.withIndex()){
+            if (text == value) {
+                indexOfArray = i
+                elements = text
+            } else {
+                Toast.makeText(this, "Элемент для удаления не найден", Toast.LENGTH_LONG).show()
+            }
+        }
+        if(indexOfArray != null){
+            textArray.removeAt(indexOfArray)
+            Toast.makeText(this, "$elements удален", Toast.LENGTH_LONG).show()
+        }
+    }
+
+    private fun displayArray() {
+        var result = ""
+        for(list in textArray){
+            result = "$result $list \n"
+        }
+        tvList.text = result
+    }
 }
-
-
 
 
 // Что такое переменная? - ячейка памяти для хранения информации
